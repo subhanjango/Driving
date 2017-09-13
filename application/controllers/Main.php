695f4c7d8f3f8cea6a727afd36a26c81f64028fb
Arman.Sheikh <arman.sheikh@cubixlabs.com>
2017-09-13 06:09:10 -0700

    test test

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -3,12 +3,13 @@
 
 		class Main extends CI_Controller {
 			function __construct()
-			{
+			{   
 				parent::__construct();
 				$data = array();
 				$this->load->database();
 				$this->load->helper('url');
 				$this->load->library(array('form_validation','session'));
+				echo "test test";
 
 			}
 			
